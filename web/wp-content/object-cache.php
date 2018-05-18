ee4d085b63616cd2c4aff836c8d01eb543f63b5d
sparkjames <jpfleiderer@gmail.com>
2018-05-18 15:43:00 -0400

    give the mu plugin idea one more chance

--- web/wp-content/object-cache.php
+++ web/wp-content/object-cache.php
@@ -1,3 +1,4 @@
 <?php
 # This is a Windows-friendly symlink
-require_once WP_CONTENT_DIR . '/plugins/wp-redis/object-cache.php';
+// require_once WP_CONTENT_DIR . '/plugins/wp-redis/object-cache.php';
+require_once WPMU_PLUGIN_DIR . '/wp-redis/object-cache.php';
