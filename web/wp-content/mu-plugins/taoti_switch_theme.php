630e2bf7e4bbf8b3fe091adeb44e91f204bb9040
sparkjames <jpfleiderer@gmail.com>
2018-05-18 16:05:07 -0400

    Add description to the theme switcher plugin

--- web/wp-content/mu-plugins/taoti_switch_theme.php
+++ web/wp-content/mu-plugins/taoti_switch_theme.php
@@ -1,4 +1,12 @@
 <?php
+/*
+  Plugin Name: Theme Switcher
+  Description: Automatically switches to the base theme when WP realizes the twentyX themes have been removed.
+  Version: 1.0
+  Author: James Pfleiderer
+  Author URI: https://www.taoti.com/
+*/
+
 // Since the twentyX themes are deleted from that repo, WordPress will normally complain that 'the twentyX theme does not exist.' until you go to the Dashboard and switch the theme.
 // This plugin will automatically determine if a theme is not set, in which case it will switch to the 'jp-base' theme. Useful for when Pantheon deploys a new site based on our custom WordPress upstream.
 
