c58e3641435c85a98ffa6ca25bf76844cee892e2
sparkjames <jpfleiderer@gmail.com>
2018-05-18 15:50:25 -0400

    update the require command for the object-cache file

--- web/wp-content/object-cache.php
+++ web/wp-content/object-cache.php
@@ -1,4 +1,6 @@
 <?php
 # This is a Windows-friendly symlink
 // require_once WP_CONTENT_DIR . '/plugins/wp-redis/object-cache.php';
-require_once WPMU_PLUGIN_DIR . '/wp-redis/object-cache.php';
+
+// require_once WPMU_PLUGIN_DIR . '/wp-redis/object-cache.php';
+require_once WP_CONTENT_DIR . '/mu-plugins/wp-redis/object-cache.php';
