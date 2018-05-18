f902aee2a9888fd3477e476b8ad47c8d619bb7eb
sparkjames <jpfleiderer@gmail.com>
2018-05-18 16:01:26 -0400

    Clean up comments, add description to my MU plugin loader

--- web/wp-content/mu-plugins/mu-loader.php
+++ web/wp-content/mu-plugins/mu-loader.php
@@ -1,4 +1,11 @@
 <?php
+/*
+  Plugin Name: Taoti Core
+  Description: Adds necessary plugins like ACF and Timber.
+  Version: 1.01
+  Author: James Pfleiderer
+  Author URI: https://www.taoti.com/
+*/
 
 require WPMU_PLUGIN_DIR.'/disable-emojis/disable-emojis.php';
 require WPMU_PLUGIN_DIR.'/jp/jp.php';
