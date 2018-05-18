9ff626c5395a2b0071b60a64f7064f91e80c6849
sparkjames <jpfleiderer@gmail.com>
2018-05-18 15:30:21 -0400

    Pantheon did not like the wp-redis plugin in the MU folder, moving it back to normal plugins folder

--- web/wp-content/mu-plugins/mu-loader.php
+++ web/wp-content/mu-plugins/mu-loader.php
@@ -4,4 +4,3 @@ require WPMU_PLUGIN_DIR.'/disable-emojis/disable-emojis.php';
 require WPMU_PLUGIN_DIR.'/jp/jp.php';
 require WPMU_PLUGIN_DIR.'/advanced-custom-fields-pro/acf.php';
 require WPMU_PLUGIN_DIR.'/timber-library/timber.php';
-require WPMU_PLUGIN_DIR.'/wp-redis/wp-redis.php';
