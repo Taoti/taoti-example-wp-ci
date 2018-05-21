2274ef7b367bd6fb3a615caa02621a183bd7e206
sparkjames <jpfleiderer@gmail.com>
2018-05-21 08:57:31 -0400

    Add other cache plugin

--- web/wp-content/mu-plugins/mu-loader.php
+++ web/wp-content/mu-plugins/mu-loader.php
@@ -12,3 +12,4 @@ require WPMU_PLUGIN_DIR.'/jp/jp.php';
 require WPMU_PLUGIN_DIR.'/advanced-custom-fields-pro/acf.php';
 require WPMU_PLUGIN_DIR.'/timber-library/timber.php';
 require WPMU_PLUGIN_DIR.'/wp-redis/wp-redis.php';
+require WPMU_PLUGIN_DIR.'/pantheon-advanced-page-cache/pantheon-advanced-page-cache.php';
