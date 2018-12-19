2058d75347c697553f00379f0b6ddeedabd18049
James Pfleiderer <jpfleiderer@gmail.com>
2018-12-19 14:46:12 -0500

    update to WP 5.0.1, update pantheon mu plugin

--- web/wp-content/object-cache.php
+++ web/wp-content/object-cache.php
@@ -1,4 +1,3 @@
 <?php
-# This is a Windows-friendly symlink
-// require_once WP_CONTENT_DIR . '/plugins/wp-redis/object-cache.php';
-require_once WP_CONTENT_DIR . '/mu-plugins/wp-redis/object-cache.php';
+# Enable the require_once below to enable the Redis feature
+// require_once WP_CONTENT_DIR . '/mu-plugins/wp-redis/object-cache.php';
