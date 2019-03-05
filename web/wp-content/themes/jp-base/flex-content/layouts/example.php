45b442fa7587ff809d29f26ca7444dadca4758f1
James Pfleiderer <jpfleiderer@gmail.com>
2019-03-05 11:55:27 -0500

    Add notes to the example layout.

--- web/wp-content/themes/jp-base/flex-content/layouts/example.php
+++ web/wp-content/themes/jp-base/flex-content/layouts/example.php
@@ -1,4 +1,7 @@
 <?php
+### Most layouts will retrieve the field values with get_sub_field() and feed those as arguments to one of the modules, then render the module.
+
+use Modules\Example;
 
 $args = array(
     'args_will_go_here' => get_sub_field('sub_field_name'),
