1c24e6b61fc8e7ea992ab5cfc12b1a346c4f5638
James Pfleiderer <jpfleiderer@gmail.com>
2019-07-08 14:59:22 -0400

    remove redis plugin

--- web/wp-content/mu-plugins/mu-loader.php
+++ web/wp-content/mu-plugins/mu-loader.php
@@ -12,6 +12,5 @@ require WPMU_PLUGIN_DIR.'/jp/jp.php';
 require WPMU_PLUGIN_DIR.'/advanced-custom-fields-pro/acf.php';
 require WPMU_PLUGIN_DIR.'/acf-better-search/acf-better-search.php';
 require WPMU_PLUGIN_DIR.'/timber-library/timber.php';
-require WPMU_PLUGIN_DIR.'/wp-redis/wp-redis.php';
 require WPMU_PLUGIN_DIR.'/pantheon-advanced-page-cache/pantheon-advanced-page-cache.php';
 require WPMU_PLUGIN_DIR.'/safe-svg/safe-svg.php';
